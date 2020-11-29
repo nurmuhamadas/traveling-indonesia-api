00cf297f9391ba6d1bfb7f68131e248d0a7a7246
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-29 09:43:28 +0700

    Change mongoose to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,25 @@
 const app = require('./server');
-const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb');
+const DestinationsModel = require('./models/destinations.model');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(
+const client = new MongoClient(
     process.env.DESTINATION_DB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
 );
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-  });
-});
+client.connect()
+    .then(() => {
+      app.listen(port, async () => {
+        await DestinationsModel.injectDB(client);
+        console.log(`Listening on port: ${port}`);
+      });
+    })
+    .catch((error) => {
+      console.log(`Faild to connect. Error: ${error}`);
+      process.exit(1);
+    });
