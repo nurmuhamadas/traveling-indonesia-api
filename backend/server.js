7bb0525fca1841124462d8a2fcf9f2b7c45c3a2a
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-12-03 17:08:24 +0700

    Modified server with nextjs

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const destination = require('./routes/destination.route');
+const next = require('next');
+require('dotenv').config();
 
+const dev = process.env.NODE_ENV !== 'production';
 const app = express();
+const nextApp = next({dev});
+const handle = nextApp.getRequestHandler();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+nextApp
+    .prepare()
+    .then(() => {
+      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({extended: true}));
+      app.use(cors());
 
-app.use('/api/v1/destinations', destination);
-app.use('/', express.static(path.join(__dirname, '../.next')));
-app.use('*', (req, res) => {
-  res.status(404).json({error: 'Request not found'});
-});
+      app.use('/api/v1/destinations', destination);
+      app.use('/', (req, res) => handle(req, res));
+    });
 
 module.exports = app;
