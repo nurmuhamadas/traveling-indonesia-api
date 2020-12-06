f74c8ee5541139b7f8b6a7692796e5079eda34e4
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-12-06 15:48:20 +0700

    Add link to navigate from card and remove unused code

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css';
 import {Card} from 'primereact/card';
 import cn from 'classname';
+import Link from 'next/link';
 
 function Cards({data}) {
   return (
@@ -20,10 +21,30 @@ function Cards({data}) {
                 src={imgSrc}
                 onError={(e) => e.target.src='https://icon-library.com/images/photo-placeholder-icon/photo-placeholder-icon-3.jpg'}
               />}
+              footer={
+                <Link href={`/destination/${destination._id}`}>
+                  <a className="button">View</a>
+                </Link>
+              }
             ></Card>
           );
         })
       }
+      <style jsx>{`
+        .button {
+          color: var(--primary-color);
+          padding: 1rem 1.5rem;
+          cursor: pointer;
+          margin-bottom: 1rem;
+          border-radius: .5rem;
+          transition: all ease-in-out .3s;
+        }
+
+        .button:hover {
+          color: #ffffff;
+          background-color: var(--secondary-color);
+        }
+      `}</style>
     </>
   );
 }
