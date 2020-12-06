e28088148a3f83d090d4c862884a05aa70b517ec
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-12-06 15:07:51 +0700

    add max-width header and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,18 +3,26 @@ import SocialIcon from './SocialIcon';
 const Footer = () => {
   return (
     <footer>
-      <p className="signature">
-        Made with
-        <img src="/icons/favorite.svg" alt="love" className="love"/>
-        by Nur Muhamad Ash Shidiqi
-      </p>
-      <SocialIcon color="white" />
+      <div className="footer">
+        <p className="signature">
+          Made with
+          <img src="/icons/favorite.svg" alt="love" className="love"/>
+          by Nur Muhamad Ash Shidiqi
+        </p>
+        <SocialIcon color="white" />
+      </div>
       <style jsx>{`
         footer {
           width: 100vw;
           height: auto;
           padding: 1.5rem 2rem;
           background-color: var(--primary-color);
+        }
+
+        .footer {
+          width: 100%;
+          max-width: 1200px;
+          margin: 0 auto;
           display: flex;
           flex-direction: column;
           justify-content: space-between;
@@ -32,7 +40,7 @@ const Footer = () => {
         }
 
         @media screen and (min-width: 768px) {
-          footer {
+          .footer {
             flex-direction: row;
             align-items: center;
           }
